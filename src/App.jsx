b973994e0a5b9226af1abd1646e85759b027796f
David Luan Gomes Lima <david0596@digitalcollegesul.local>
2024-06-05 16:22:57 -0300

    inclusão dos comentários para entendimento do código

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import './App.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react' //Importa os hook's para uso no código
 import Table from 'react-bootstrap/Table'
 
 function App() {
 
-  const [dados, setDados] = useState([])
+  const [dados, setDados] = useState([]) //usado para definição do estado que será usado.
 
-  useEffect(() => {
+  useEffect(() => { //usado para consumo da API através do uso de Try/Catch
     try {
+      //Função criada para consumir a API
       async function dadosBinance (){
-        let response = await fetch("https://testnet.binance.vision/api/v1/klines?symbol=BTCUSDT&interval=1m")
-        let dados = await response.json()
-        setDados(dados)
-        console.log(dados)
+        let response = await fetch("https://testnet.binance.vision/api/v1/klines?symbol=BTCUSDT&interval=1m") //Fetch com parâmetros para consumo da API
+        let dados = await response.json() //Transição dos dados para JSON
+        setDados(dados) //Esta pegando o estado dos dados atuais que estão sendo enviados pela API
+        console.log(dados) //Confirmando a impressão no console dos dados que estão sendo consumidos da API
     }
-    dadosBinance()
+    dadosBinance() //Chama a função
   } catch {
-      console.log("Erro")
+      console.log("Erro") //Passa a mensagem de erro no console, caso não sejam consumidos os dados
     }
     
   }, [])
@@ -36,11 +37,15 @@ function App() {
             </tr>
           </thead>
           <tbody>
+            {/* O map está sendo utilizado para pecorrer os dados do array que está sendo consumido na API */}
           {dados.map((item, index) => {
-            if (index >= 400 && index <= 443) { // Verifica se é o último item
+            {/*O index será usado como a chave do arrey para identificação dos componentes que serão utilizados na tabela*/}
+            if (index >= 400 && index <= 443) { // Pecorre dos dados dos arreys entre da posição 400 a 443
               return (
                 <tr key={index}>
-                  <td>{new Date(item[0]).toLocaleString()}</td>
+                  {/*configura o dado do index 0 como uma data */}
+                  <td>{new Date(item[0]).toLocaleString()}</td> 
+                  {/*consome nas td's os index informados vinculando ao parametro item que está sendo declarado*/}
                   <td>{item[1]}</td>
                   <td>{item[2]}</td>
                   <td>{item[3]}</td>
